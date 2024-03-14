c972eb6e970b909cddee21ae0b809c0c31e068c4
yaozm <ityaozm@gmail.com>
2024-03-14 14:39:02 +0800

    chore(deps): update Illuminate and Orchestra dependencies versions
    - Updated Illuminate dependencies to versions ^7.0 || ^8.0 || ^9.0 || ^10.0 || ^11.0
    - Updated Orchestra/testbench dependency to versions ^5.0 || ^6.0 || ^7.0 || ^8.0 || ^9.0

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -25,7 +25,7 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
     require __DIR__.'/vendor/autoload.php';
-    $mbConfig->defaultBranch('main');
+    $mbConfig->defaultBranch('master');
 
     /*
      * release workers - in order to execute
